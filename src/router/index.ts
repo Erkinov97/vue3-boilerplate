467c0d2a08cb533f7f69587070339b59b34ba3d9
unknown <erkinov997@gmail.com>
2024-06-05 09:53:27 +0500

    feat: upgrade vue-router version and fix some settings

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import routes from "./routes";
 import { globalMiddleware } from "./middleware";
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
 
